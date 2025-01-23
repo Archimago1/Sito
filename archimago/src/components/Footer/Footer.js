6d7a541f590c36f666dd6ef2eb1b4cc3b9eeb151
Archimago1 <collaboratori@archimago.net>
2025-01-23 16:57:16 +0100

    up

--- archimago/src/components/Footer/Footer.js
+++ archimago/src/components/Footer/Footer.js
@@ -2,10 +2,15 @@ import { Container, Navbar } from "react-bootstrap";
 import "./styles.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear(); // Si ottiene l'anno corrente
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary sticky-bottom" id="footer">
       <Container className="d-flex justify-content-end">
-        <Navbar.Brand id="title">© 2025 by Alice Ibba</Navbar.Brand>
+        <Navbar.Brand id="title">
+          Archimago srl | P.IVA 10147120017 | © {currentYear} | All rights
+          reserved | Site by Alice Ibba
+        </Navbar.Brand>
       </Container>
     </Navbar>
   );
