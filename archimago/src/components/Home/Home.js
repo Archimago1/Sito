7579a80309b8acd3ba8aca7f63c339526ce7e2ed
Archimago1 <collaboratori@archimago.net>
2025-01-28 17:40:18 +0100

    up

--- archimago/src/components/Home/Home.js
+++ archimago/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../Navbar/Navbar.js";
 import Footer from "../Footer/Footer.js";
+import photo1 from "../../assets/photo1.png";
 
 import { Container, Row, Col, Image, Button, Stack } from "react-bootstrap";
 import "./styles.css";
@@ -9,7 +10,22 @@ function Home() {
   return (
     <>
       <NavBar />
-      
+      <Container>
+        <Row className="d-flex justify-content-center align-items-center">
+          <Col
+            lg={4}
+            xs={12}
+            className="d-flex justify-content-center align-items-center"
+            id="photo1"
+          >
+            <Image
+              src={photo1}
+              id="photo"
+              className="align-self-center justify-self-center"
+            />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
